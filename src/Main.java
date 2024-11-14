282ed1a5ab7fabf1c29e765d2bb61989d7f31478
DonMB77 <purposefuldrifter@gmail.com>
2024-11-14 01:01:57 +0100

    Created Project + JFrame and a testing Array of numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main (String[] args) {
+        JFrame jframe = new JFrame("Sudoku Solver");
+
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        jframe.setSize(600,600);
+
+        jframe.setVisible(true);
+
+        int[] testSudokuField = {
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8,
+                1 ,3 ,6 ,9 ,5 ,7 ,4 ,2 ,8
+        };
+
+        for (int number : testSudokuField) {
+            
+        }
+
+        JLabel label = new JLabel();
+        label.setText(String.valueOf(testSudokuField[0]));
+
+        jframe.add(label);
     }
 }
---
+++ src/SudokuFieldGenerator.java
@@ -0,0 +1,8 @@
+public class SudokuFieldGenerator {
+
+    int[] sudokuNumbersInField = new int[81];
+
+    public static void generateSudokuField () {
+
+    }
+}
